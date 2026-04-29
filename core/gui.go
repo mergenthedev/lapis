bd0b4b5363ad61358d6b4af342687b8b35896dc7
Berkay <mergenthedev@proton.me>
2026-04-29 23:03:49 +0300

    start fixing

--- core/gui.go
+++ core/gui.go
@@ -1,18 +1,1 @@
 package core
-
-/*
-#cgo CXXFLAGS: -I.
-#cgo LDFLAGS: -lstdc++ -lm
-#cgo CXXFLAGS: -std=c++17
-#cgo LDFLAGS: -lGL -lglfw
-
-#include <stdio.h>
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
-*/
-import "C"
-
-func InitGUI() {
-
-}
